18cf45ce6039ce14ad0c48b7b8cf8f1946380ea5
Twory <deadlus@gmail.com>
2016-09-07 14:54:33 +0200

    Added aldeed-collection2. Moved schema validation to validate.js

--- imports/ui/components/index.jsx
+++ imports/ui/components/index.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
 
-export const Index = () => <h3>Index</h3>;
+//export const Index = () => <h3>Index</h3>;
+export const Index = () => (
+    <h3>A form will appear here shortly</h3>
+)
