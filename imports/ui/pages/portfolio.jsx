c938b46485c47e597105af6d837ef51e9f2db1df
Twory <deadlus@gmail.com>
2016-10-05 10:24:17 +0200

    Fixed a little styling issue in portfolio.jsx (removed o-pack class)

--- imports/ui/pages/portfolio.jsx
+++ imports/ui/pages/portfolio.jsx
@@ -4,17 +4,26 @@ export default class Portfolio extends Component {
     render() {
         return(
             <div className="o-box-negative-margin">
-                <div className="o-pack">
-                    <div className="o-pack__item o-box--small thumbnail u-1/4">
+                <div className="">
+                    <div className=" o-box--small thumbnail u-1/4">
                         <img src="/img/1.jpg" className="o-media" />
                     </div>
-                    <div className="o-pack__item o-box--small thumbnail u-1/4">
+                    <div className=" o-box--small thumbnail u-1/4">
                         <img src="/img/1.jpg" className="o-media" />
                     </div>
-                    <div className="o-pack__item  o-box--small thumbnail u-1/4">
+                    <div className="  o-box--small thumbnail u-1/4">
                         <img src="/img/1.jpg" className="o-media" />
                     </div>
-                    <div className="o-pack__item  o-box--small thumbnail u-1/4">
+                    <div className="  o-box--small thumbnail u-1/4">
+                        <img src="/img/1.jpg" className="o-media" />
+                    </div>
+                    <div className=" o-box--small thumbnail u-1/4">
+                        <img src="/img/1.jpg" className="o-media" />
+                    </div>
+                    <div className=" o-box--small thumbnail u-1/4">
+                        <img src="/img/1.jpg" className="o-media" />
+                    </div>
+                    <div className="  o-box--small thumbnail u-1/4">
                         <img src="/img/1.jpg" className="o-media" />
                     </div>
                 </div>
