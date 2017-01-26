eae841c4b043613c53b7cca982b2654000e2715d
Twory <deadlus@gmail.com>
2017-01-26 19:57:23 +0100

    Clean up of unused code and files

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -8,22 +8,15 @@ import PostsList from '../../ui/components/postsList.jsx';
 import Admin from '../../ui/components/admin.jsx';
 import Post from '../../ui/components/post.jsx';
 
-import { App } from '../../ui/layouts/app.jsx';
-import Portfolio from '../../ui/pages/portfolio.jsx';
-import { Studio } from '../../ui/pages/studio.jsx';
-import { Blog } from '../../ui/pages/blog.jsx';
-import { Contact } from '../../ui/pages/contact.jsx';
 import { NotFound } from '../../ui/pages/notFound.jsx';
 
-
+import { App } from '../../ui/layouts/app.jsx';
 
 Meteor.startup( () => {
 	render(
 		<Router history={ browserHistory }>
 			<Route path="/" component={ App } >
 				<IndexRoute component={ Index } />
-				<Route path="/portfolio" component={ Portfolio } />
-				<Route path="/studio" component={ Studio } />
 				<Route path="/admin" component={ Admin } />
 				<Route path="/editor" component={ Editor } />
 				<Route path="/postsList" component={ PostsList } />
--- imports/ui/pages/hello.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const Hello = ( { params, location } ) => (
-	<h3>Howdy, { params.name }! You like { location.query.food }.</h3>
-	// use params in links: (...)/hello/name?food=somefood
-);
